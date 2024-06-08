9c0a4e39087b3b3a2c870985988fb1eb0486115d
Mayara Martiniano <mahyrece@gmail.com>
2024-06-07 21:21:22 -0300

    tela de login validada

---
+++ src/components/Button/styles.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const ContainerButton = styled.button `
+    width: 100%;
+    height: 52px;
+
+    border: 0;
+    border-radius: 5px;
+    border: 5px;
+    background-color: #fcb900 ;
+    font-family: "Road Rage", sans-serif;
+    font-size: 30px;
+    color: #fff;
+
+    &:hover{
+        background-color: #fcb900;
+        
+    }
+    
+
+`
---
+++ src/containers/Login/index.jsx
@@ -0,0 +1,62 @@
+
+import { useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from 'yup'
+
+import Logo from '../../assets/parrilos1.png'
+import { Button } from '../../components/Button'
+import { Container, RightContainer, Title, Form, InputContainer, LeftContainer } from './styles'
+
+
+
+
+export function Login() {
+    const schema = yup
+        .object({
+            email: yup.string().email('Digite um e-mail válido').required('O email é obrigatório'),
+            password: yup.string().min(6, 'A senha deve ter pelo menos 6 caracteres').required('Digite uma senha'),
+        })
+        .required();
+
+    const { register, handleSubmit, formState: { errors } } = useForm({
+        resolver: yupResolver(schema),
+
+          });
+          console.log(errors)
+
+   
+
+    const onSubmit = (data) => console.log(data)
+
+    return (
+        <Container>
+            <LeftContainer>
+                <img src={Logo} alt='logo-devburger' />
+            </LeftContainer>
+            <RightContainer>
+                <Title>
+                    Olá, seja bem vindo ao <span>Parrilos!</span>
+                    <br />
+                    Acesse com seu <span>Login e senha.</span>
+                </Title>
+                <Form onSubmit={handleSubmit(onSubmit)}>
+                    <InputContainer>
+                        <label>Email</label>
+                        <input type='email' {...register('email')} />
+                        <p>{errors?.email?.message}</p> 
+                    </InputContainer>
+
+                    <InputContainer>
+                        <label>Senha</label>
+                        <input type='password' {...register('password')} />
+                        <p>{errors?.password?.message}</p>
+                    </InputContainer>
+
+                    <Button type="submit">Entrar</Button>
+                </Form>
+                <p>Não possui conta? <a>Clique aqui.</a> </p>
+            </RightContainer>
+        </Container>
+    )
+
+}
