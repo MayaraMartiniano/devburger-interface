e4e60765e017d57ca6bca4602c91b47bab578552
Mayara Martiniano <mahyrece@gmail.com>
2024-09-24 22:07:01 -0300

    tela de cardapio parte 1

--- src/components/CardProduct/styles.js
+++ src/components/CardProduct/styles.js
@@ -10,6 +10,7 @@ export const Container = styled.div `
     background-color: #ffffff;
     cursor: grab; //mãozinha que arrasta
     box-shadow: rgba(0,0,0,0.35) 0px 5px 15px;
+    position: relative;
 
     div {
         width: 100%;
--- src/components/CategoriesCarousel/index.jsx
+++ src/components/CategoriesCarousel/index.jsx
@@ -15,7 +15,7 @@ export function CategoriesCarousel() {
             
             
             setCategories(data)
-            console.log(data)
+            
 
         }
 
